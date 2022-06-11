b682619ae8dedb9390d7a327c268ac3a0e56981a
waseem <mohammad.siddiqui@hof-university.de>
2022-06-11 17:46:11 +0200

    feat: Check for double quoted text

--- Program.cs
+++ Program.cs
@@ -11,23 +11,36 @@ namespace ExocadWordBreaker
         {
             string userStr;            
             Console.WriteLine("Please enter your sentence:");
-            userStr =Console.ReadLine();            
-            string[] UserStrArr = userStr.Split(new char[]{' ','.',',','<','>','?','/',':',';',']','[','{','}','|','!','@','#','$','%','^','&','*','(',')','_','-','+','='}, StringSplitOptions.RemoveEmptyEntries);
+            userStr =Console.ReadLine();
+
+            string[] UserStrArr = userStr.Split(new char[]{' ','.',',','<','>','?','/',':',';',']','[','{','}','|','!','@','#','$','%','^','&','*','(',')','_','-','+','='}, StringSplitOptions.RemoveEmptyEntries);            
+            
             CounterParam(UserStrArr);
         }
 
         static void CounterParam(string[] UserStrArr)
-        {
-            Dictionary<int, string> wordcounts= new Dictionary<int, string>();              
-            foreach (string item in UserStrArr)
-            {
-                if (!item.Any(char.IsDigit) || item.All(char.IsDigit))
+        {            
+            Dictionary<int, string> wordcounts= new Dictionary<int, string>();
+            string temp= String.Empty;
+                               
+            foreach(var item in UserStrArr)            
+            {         
+                Console.WriteLine(item);                    
+                if (temp!=String.Empty)
                 {
-                    if (!wordcounts.ContainsKey(item.Length))                                            
-                        wordcounts.Add(item.Length, item);                    
-                    else                    
-                        wordcounts[item.Length]+=";" + item;                    
+                    if (item.EndsWith('"'))
+                    {
+                        temp=temp +" " +item;                        
+                        CheckKeyAndWordToDic(temp.Substring(1, temp.Length-2), wordcounts);                        
+                        temp= String.Empty;                        
+                    }
+                    else
+                        temp=temp +" " +item;
                 }
+                else if (item.StartsWith('"'))                
+                    temp = item;                
+                else                
+                    CheckKeyAndWordToDic(item, wordcounts);                                   
             }
 
             var sortedKeys = wordcounts.Keys.ToList();
@@ -36,11 +49,22 @@ namespace ExocadWordBreaker
 
             for (int i = 1; i <= MaxCharacter; ++i)
             {
-                if (wordcounts.ContainsKey(i))                
-                    Console.WriteLine("words with {0} letters occured {1} times (words={2})", i,wordcounts[i].Split(';').Length, wordcounts[i]);                                    
+                if (wordcounts.ContainsKey(i))                              
+                    Console.WriteLine("words with {0} letters occured {1} times (words={2})", i,wordcounts[i].Split(';').Length, wordcounts[i]);                                                        
                 else
                     Console.WriteLine("words with {0} letter did not occur",i);
-            }            
+            }
         }
-     }
+
+        static void CheckKeyAndWordToDic(string word, Dictionary<int, string> wordcounts)
+        {
+            if (!word.Any(char.IsDigit) || word.All(char.IsDigit))
+            {
+                if (!wordcounts.ContainsKey(word.Length))
+                    wordcounts.Add(word.Length, word);
+                else
+                    wordcounts[word.Length]+=";" + word;
+            }
+        }
+    }
 }
